a4b632f46c3d414ac2c548c8526470e4639c76ac
Chris Huang <102665605+a950523a@users.noreply.github.com>
2026-01-28 16:38:52 +0800

    V1_Beta

--- components/port_esp32/src/hal_impl.cpp
+++ components/port_esp32/src/hal_impl.cpp
@@ -179,12 +179,14 @@ private:
 
     void initUart() {
         uart_config_t uart_config = {
-            .baud_rate = 115200,
+            .baud_rate = UART_BAUD_RATE,
             .data_bits = UART_DATA_8_BITS,
             .parity    = UART_PARITY_DISABLE,
             .stop_bits = UART_STOP_BITS_1,
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .rx_flow_ctrl_thresh = 122,
             .source_clk = UART_SCLK_DEFAULT,
+            .flags = 0,
         };
         uart_driver_install(UART_PORT_NUM, 1024, 0, 0, NULL, 0);
         uart_param_config(UART_PORT_NUM, &uart_config);
